92a9bae5a00ef7c6c4ba63ebe621412deb3c2c25
weslley daeski <weslley.daeski@escola.pr.gov.br>
2026-05-05 09:18:58 -0300

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,8 +1,6 @@
-import { registerRootComponent } from 'expo';
-
+import { registerRootComponent} fromm'expo';
 import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
+// registerComponent('main' , () => App);
+// It also ensures that whether you load the app in Expo Go or in a native biild,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootCompnent(App);
